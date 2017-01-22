a08aa6a4f097e5662aee0efa44c1300788dc4880
Chris Wells Wood <cwwoodesq@gmail.com>
2017-01-22 17:50:57 +0000

    Setup up so that the app is a module.

---
+++ ccbmk2/__init__.py
@@ -0,0 +1,1 @@
+from .ccbmk2 import app
---
+++ ccbmk2/config/default.py
@@ -0,0 +1,2 @@
+BCRYPT_LEVEL = 12  # Configuration for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "chris.wood@bristol.ac.uk"  # For use in application emails
