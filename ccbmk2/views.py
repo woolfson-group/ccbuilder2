bf56057ae0ae660a2216b4bb1f877b9cdf2b9ac5
Chris Wells Wood <Chris Wells Wood>
2017-01-30 19:00:59 +0000

    Added module and level and function level docs.

--- ccbmk2/views.py
+++ ccbmk2/views.py
@@ -1,3 +1,5 @@
+"""Views for CCBuilder Mk.II"""
+
 import flask
 
 from ccbmk2 import app
@@ -5,4 +7,5 @@ from ccbmk2 import app
 
 @app.route('/')
 def builder():
+    """Main view for the builder interface."""
     return flask.render_template('builder.html')
