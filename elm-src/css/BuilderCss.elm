15d55079cddcc60a79eae717d7ef001601abb0c0
Chris Wells Wood <cwwoodesq@gmail.com>
2017-02-28 11:55:50 +0000

    Started elm css file for builder.

--- elm-src/css/BuilderCss.elm
+++ elm-src/css/BuilderCss.elm
@@ -1,45 +1,55 @@
 module BuilderCss exposing (..)
 
 import Css exposing (..)
-import Css.Elements exposing (body, li)
-import Css.Namespace exposing (namespace)
+import Css.Elements exposing (body, h1, h2, h3, table)
 
 
 type CssClasses
-    = NavBar
+    = OverlayPanel
+    | PanelToggle
+    | ParameterInput
 
 
 type CssIds
     = Page
 
 
+css : Stylesheet
 css =
-    (stylesheet << namespace "dreamwriter")
+    stylesheet
     [ body
-        [ overflowX auto
-        , minWidth (px 1280)
+        [ color colorPalette.c5
+        , margin (px 0)
+        , padding (px 0)
         ]
-    , id Page
-        [ backgroundColor (rgb 200 128 64)
-        , color (hex "CCFFFF")
-        , width (pct 100)
-        , height (pct 100)
-        , boxSizing borderBox
-        , padding (px 8)
-        , margin zero
+    , h1
+        [ margin (px 0)
+        , paddingLeft (px 10)
         ]
-    , class NavBar
-        [ margin zero
-        , padding zero
-        , children
-            [ li
-                [ (display inlineBlock) |> important
-                , color primaryAccentColor
-                ]
-            ]
+    , each [ h2, h3 ]
+        [ marginTop (px 2)
+        , marginBottom (px 2)
+        ]
+    , Css.Elements.table
+        [ borderCollapse collapse
         ]
     ]
 
 
-primaryAccentColor =
-    hex "ccffaa"
+type alias ColorPalette =
+    { c1 : Css.Color
+    , c2 : Css.Color
+    , c3 : Css.Color
+    , c4 : Css.Color
+    , c5 : Css.Color
+    }
+
+
+colorPalette : ColorPalette
+colorPalette =
+    { c1 = hex "e6e8e6"
+    , c2 = hex "ced0ce"
+    , c3 = hex "9fb8ad"
+    , c4 = hex "475841"
+    , c5 = hex "3f403f"
+    }
