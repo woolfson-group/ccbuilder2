f0bce4a43278862909824c4f6007b09cc57130e5
Chris Wells Wood <cwwoodesq@gmail.com>
2017-02-28 11:11:57 +0000

    Moved location of elm source to outside of the app.

--- tools/dev_build.sh
+++ tools/dev_build.sh
@@ -1,2 +1,2 @@
-elm-css ccbmk2/elm-src/css/Stylesheets.elm --output ccbmk2/static/css/
-elm-make ./ccbmk2/elm-src/Builder.elm --output=./ccbmk2/static/elm/builder.js --debug
+elm-css ./elm-src/css/Stylesheets.elm --output=./ccbmk2/static/css/
+elm-make ./elm-src/Builder.elm --output=./ccbmk2/static/elm/builder.js --debug
